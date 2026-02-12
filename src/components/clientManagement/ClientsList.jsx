e2ab994eb308ef8389933944c732a97a31a77ccd
Anuj2508 <anuj.y@ecodedash.com>
2026-02-12 17:34:34 +0530

    Latest code committed with .env file change

--- src/components/clientManagement/ClientsList.jsx
+++ src/components/clientManagement/ClientsList.jsx
@@ -73,7 +73,7 @@ const ClientList = () => {
   });
   const [statusOptions, setStatusOptions] = useState([]);
 
-  const API_BASE_URL = "http://localhost:5000/api";
+  const API_BASE_URL = "https://crm-backend-qbz0.onrender.com/api";
   const token = localStorage.getItem("token");
 
   useEffect(() => {
