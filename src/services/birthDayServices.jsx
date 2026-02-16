e16aef6b23c8b8a7e6e01a19c1f2100c784f86a3
Anuj2508 <anuj.y@ecodedash.com>
2026-02-16 11:48:57 +0530

    Birthday ocde committed

--- src/services/birthDayServices.jsx
+++ src/services/birthDayServices.jsx
@@ -2,7 +2,7 @@ import { fetchHandler } from "../fatchHandler/fetchHandler";
 
 export const getBirthday = ({ year, month }) => {
   return fetchHandler(
-    `/dashboard/birthdays?month=${month}&year=${year}`,
+    `/api/dashboard/birthdays?month=${month}&year=${year}`,
     "GET",
   );
 };
