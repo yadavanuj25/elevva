dbbf37f9b0d507001db5edac7e160173b3e527d8
Anuj2508 <anuj.y@ecodedash.com>
2026-02-18 12:20:38 +0530

    Changed the error show time in message context file

--- src/auth/MessageContext.jsx
+++ src/auth/MessageContext.jsx
@@ -5,11 +5,11 @@ export const MessageProvider = ({ children }) => {
   const [errorMsg, setErrorMsg] = useState("");
   const showSuccess = (msg) => {
     setSuccessMsg(msg);
-    setTimeout(() => setSuccessMsg(""), 6000);
+    setTimeout(() => setSuccessMsg(""), 5000);
   };
   const showError = (msg) => {
     setErrorMsg(msg);
-    setTimeout(() => setErrorMsg(""), 6000);
+    setTimeout(() => setErrorMsg(""), 5000);
   };
 
   return (
