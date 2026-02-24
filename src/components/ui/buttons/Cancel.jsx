aab3f88ff7b3ffa15bf86b01a44a3e3892058f3d
Anuj2508 <anuj.y@ecodedash.com>
2026-02-24 11:40:23 +0530

    Updated the cancel button

--- src/components/ui/buttons/Cancel.jsx
+++ src/components/ui/buttons/Cancel.jsx
@@ -18,11 +18,7 @@ const CancelButton = ({ to, onClick, label = "Cancel", className = "" }) => {
     <button
       type="button"
       onClick={handleCancel}
-      className=//   text-sm font-medium bg-gray-300  text-gray-900 hover:bg-gray-400 //   px-2 py-1 rounded //   inline-flex items-center gap-2 // {`
-      //   transition-all
-      //   ${className}
-      // `}
-      {`px-4 py-2 bg-accent-dark backdrop-blur-sm text-white  rounded-lg font-semibold hover:bg-accent-light hover:text-accent-dark shadow-md  transition duration-300 ease-in-out inline-flex items-center gap-2 ${className}`}
+      className={`px-4 py-2 bg-gray-500 backdrop-blur-sm text-white  rounded-lg font-semibold hover:bg-accent-light hover:text-accent-dark shadow-md  transition duration-300 ease-in-out inline-flex items-center gap-2 ${className}`}
     >
       <X size={18} />
       {label}
